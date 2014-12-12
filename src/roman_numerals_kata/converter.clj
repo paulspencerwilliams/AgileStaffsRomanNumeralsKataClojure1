e38db22dc76bcb318e09d832c6369cc892d02eb0
Paul Williams <will@tootingbec.office.blackpepper.co.uk>
2014-12-12 13:10:28 +0000

    works for V

--- src/roman_numerals_kata/converter.clj
+++ src/roman_numerals_kata/converter.clj
@@ -1,4 +1,11 @@
 (ns roman_numerals_kata.converter)
 
 ;;; converts roman numeral strings to their decimal equivalent
-(defn roman-to-decimal [roman] (count roman))
+(defn roman-to-decimal [roman]
+  (if (.contains roman "V") 
+    (if (.contains roman "I") 
+      (if (= (.substring roman 0 1) "I")
+        4
+        (+ 5 (- (count roman) 1)))
+      5) 
+    (count roman)))
--- test/roman_numerals_kata/converter_test.clj
+++ test/roman_numerals_kata/converter_test.clj
@@ -3,8 +3,12 @@
   (:use [roman_numerals_kata.converter]))
 
 (facts "about `roman-to-decimal`"
-  (fact "it handles I"
-    (roman-to-decimal "I") => 1
-    (roman-to-decimal "II") => 2
-    (roman-to-decimal "III") => 3))
+       (fact "it handles I"
+             (roman-to-decimal "I") => 1
+             (roman-to-decimal "II") => 2
+             (roman-to-decimal "III") => 3)
 
+       (fact "it handles V"
+             (roman-to-decimal "IV") => 4 
+             (roman-to-decimal "V") => 5
+             (roman-to-decimal "VI") => 6))
