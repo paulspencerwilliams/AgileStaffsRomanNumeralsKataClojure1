f29cdb6920d3ab69a7aad83a4ba8634182cde615
Paul Williams <paul@paulswilliams.me.uk>
2014-12-13 19:09:53 +0000

    refactored with better variable names, and inlined numeral to value map and lookup function

--- src/roman_numerals_kata/converter.clj
+++ src/roman_numerals_kata/converter.clj
@@ -1,23 +1,14 @@
 (ns roman_numerals_kata.converter)
 
-(def numeral-values {"I" 1
-                     "V"  5
-                     "X"  10
-                     "L"  50
-                     "C"  100
-                     "D"  500
-                     "M"  1000})  
-
-;;; converts roman numeral strings to their decimal equivalent
-(defn lookup [r] (numeral-values (str r)))
-
-(defn roman-to-decimal [roman]
-  (loop [d 0
-         l 0 
-         s (map lookup (reverse (seq roman)))]
-    (if-not (empty? s)
-      (let [n (first s)]
-        (if (< n l) 
-          (recur (- d n) l (rest s))
-          (recur (+ d n) n (rest s))))
-      d)))
+(defn roman-to-decimal [roman-numeral]
+  (loop [cur-total 0
+         highest-value 0 
+         values-backwards (map 
+                            #({\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000} %)
+                            (reverse (seq roman-numeral)))]
+    (if-not (empty? values-backwards)
+      (let [cur-value (first values-backwards)]
+        (if (< cur-value highest-value) 
+          (recur (- cur-total cur-value) highest-value (rest values-backwards))
+          (recur (+ cur-total cur-value) cur-value (rest values-backwards))))
+      cur-total)))
